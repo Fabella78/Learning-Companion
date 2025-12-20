46e1c7b0e8db528bd98a3f87a7a374acd440e726
Fabella78 <voidedparacosm@gmail.com>
2025-12-20 10:13:36 -0600

    Add finishSetup function for account creation

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function goToSetup() {
 function signIn() {
   alert("Sign in coming next!");
 }
+function finishSetup() {
+  const name = document.getElementById("newUsername").value;
+  const pin = document.getElementById("newPin").value;
+
+  if (name === "" || pin === "") {
+    alert("Please enter a name and PIN");
+    return;
+  }
+
+  localStorage.setItem("username", name);
+  localStorage.setItem("pin", pin);
+
+  alert("Account created! Please sign in.");
+
+  window.location.href = "index.html";
+}
